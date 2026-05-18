9aaf7025f4babb76168d2ca437b131e1b520c53a
andresdtcity <43972903+andresdtcity@users.noreply.github.com>
2026-05-18 19:31:58 -0300

    respuesta

--- script.js
+++ script.js
@@ -37,10 +37,39 @@ function mostrarPregunta(){
   });
 }
 
-function responder(seleccion,correcta){
+let respondida = false;
 
-  if(seleccion===correcta){
+function responder(btn, seleccion, correcta) {
+
+  if (respondida) return;
+
+  respondida = true;
+
+  const botones =
+    document.querySelectorAll(".opcion");
+
+  botones.forEach(b => {
+    b.disabled = true;
+  });
+
+  if (seleccion === correcta) {
+    btn.classList.add("correcta");
     puntaje++;
+  }
+  else {
+
+    btn.classList.add("incorrecta");
+
+    botones.forEach(b => {
+      if (b.innerText === correcta) {
+        b.classList.add("correcta");
+      }
+    });
+
+    errores.push({
+      pregunta: preguntas[indice].nombre,
+      correcta: correcta
+    });
   }
 }
 
