5ef2ad358fb97f0f0616f9433a0fe5ccc3803876
andresdtcity <43972903+andresdtcity@users.noreply.github.com>
2026-05-18 18:48:03 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,87 @@
+let preguntas = [];
+let indice = 0;
+let puntaje = 0;
+
+fetch("direcciones.json")
+.then(r=>r.json())
+.then(data=>{
+  preguntas = data.sort(()=>Math.random()-0.5).slice(0,25);
+  mostrarPregunta();
+});
+
+function mostrarPregunta(){
+
+  const p = preguntas[indice];
+
+  document.getElementById("contador").innerText =
+    `${indice+1}/25`;
+
+  document.getElementById("pregunta").innerText =
+    `¿Dónde se encuentra ${p.nombre}?`;
+
+  let opciones = generarOpciones(p.direccion);
+
+  let cont = document.getElementById("opciones");
+  cont.innerHTML = "";
+
+  opciones.forEach(op=>{
+
+    let btn = document.createElement("button");
+
+    btn.className = "opcion";
+    btn.innerText = op;
+
+    btn.onclick = ()=>responder(op,p.direccion);
+
+    cont.appendChild(btn);
+  });
+}
+
+function responder(seleccion,correcta){
+
+  if(seleccion===correcta){
+    puntaje++;
+  }
+}
+
+function siguiente(){
+
+  indice++;
+
+  if(indice<preguntas.length){
+    mostrarPregunta();
+  }
+  else{
+    document.getElementById("contenedor").innerHTML =
+      `<h2>Resultado: ${puntaje}/25</h2>`;
+  }
+}
+
+function generarOpciones(correcta){
+
+  const calles = [
+    "Av. Cazón",
+    "Mitre",
+    "Lavalle",
+    "Ruta 27",
+    "Belgrano",
+    "Sarmiento"
+  ];
+
+  let falsas = [];
+
+  while(falsas.length<2){
+
+    let calle =
+      calles[Math.floor(Math.random()*calles.length)];
+
+    let numero =
+      Math.floor(Math.random()*5000)+1;
+
+    falsas.push(`${calle} ${numero}`);
+  }
+
+  let opciones = [...falsas,correcta];
+
+  return opciones.sort(()=>Math.random()-0.5);
+}
