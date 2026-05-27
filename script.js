a792b44e44031c859d529df2eaaa8cfc66725d88
andresdtcity <andresdtcity@hotmail.com>
2026-05-27 18:26:37 -0300

    cambio

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let preguntas = [];
 let indice = 0;
+let respondida = false;
+let puntaje = 0;
 
 fetch("direcciones.json")
 .then(r => r.json())
@@ -16,7 +18,30 @@ function comenzarExamen() {
     mostrarPregunta();
 }
 
+function siguiente() {
+
+    if (!respondida) {
+        alert("Seleccioná una respuesta");
+        return;
+    }
+
+    indice++;
+
+    if (indice < preguntas.length) {
+
+        mostrarPregunta();
+
+    } else {
+
+        document.getElementById("contenedor").innerHTML =
+            "<h2>Examen terminado</h2>" +
+            "<h3>Puntaje: " + puntaje + "/" + preguntas.length + "</h3>";
+    }
+}
+
 function mostrarPregunta() {
+
+    respondida = false;
 
     let p = preguntas[indice];
 
@@ -27,16 +52,54 @@ function mostrarPregunta() {
 
     cont.innerHTML = "";
 
-    let opciones = [p.direccion];
+    // respuestas falsas
+    let falsas = [];
+
+    while (falsas.length < 2) {
+
+        let random =
+            preguntas[Math.floor(Math.random() * preguntas.length)].direccion;
+
+        if (
+            random !== p.direccion &&
+            !falsas.includes(random)
+        ) {
+            falsas.push(random);
+        }
+    }
+
+    // mezclar opciones
+    let opciones = [
+        p.direccion,
+        ...falsas
+    ];
+
+    opciones.sort(() => Math.random() - 0.5);
 
     opciones.forEach(op => {
 
         let btn = document.createElement("button");
 
+        btn.className = "opcion";
+
         btn.innerText = op;
 
-        btn.onclick = function() {
-            alert("Respuesta seleccionada");
+        btn.onclick = function () {
+
+            if (respondida) return;
+
+            respondida = true;
+
+            if (op === p.direccion) {
+
+                btn.style.background = "green";
+                puntaje++;
+
+            } else {
+
+                btn.style.background = "red";
+
+            }
         };
 
         cont.appendChild(btn);
